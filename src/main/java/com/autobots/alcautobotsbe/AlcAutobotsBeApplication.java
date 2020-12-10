6289382a008b6a93e9cd20df1e966c00de4967fd
atlroc99 <tyaniceluv89@gmail.com>
2020-12-10 17:13:59 -0500

    first commit

---
+++ src/main/java/com/autobots/alcautobotsbe/AlcAutobotsBeApplication.java
@@ -0,0 +1,13 @@
+package com.autobots.alcautobotsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcAutobotsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlcAutobotsBeApplication.class, args);
+    }
+
+}
